975cb8762ef0a4255dd32b80217ce9a19e8c042c
1winhyun <jimking1@naver.com>
2025-10-27 18:03:39 +0900

    feat: mvc 패턴 구현 방식을 사용한 구현 중 application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,12 @@
 package racingcar;
 
 import java.util.List;
+/*import racingcar.anotherway.domain.controller.RacingController;
+import racingcar.anotherway.domain.rule.DefaultMoveRule;
+import racingcar.anotherway.service.RacingGameService;
+import racingcar.anotherway.service.RandomNumberGenerator;
+import racingcar.anotherway.view.InputView;
+import racingcar.anotherway.view.OutputView;*/
 
 public class Application {
     public static void main(String[] args) {
@@ -19,5 +25,17 @@ public class Application {
 
         List<String> winners = RaceResult.pickWinners(cars);
         OutputView.printWinners(winners);
+
+        // anotherway 패키지 내 구현된 mvc 패턴 형식에서의 application 구현 코드입니다.
+        /*InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        DefaultMoveRule defaultMoveRule = new DefaultMoveRule();
+
+        RacingGameService racingGameService = new RacingGameService(randomNumberGenerator, defaultMoveRule);
+        RacingController racingController = new RacingController(inputView, outputView, racingGameService);
+
+        racingController.run();*/
     }
 }
